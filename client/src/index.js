8f92e7f31ec69561735864448baf1dcb16182426
franco <francoadrian.coll2012@gmail.com>
2022-12-14 12:53:54 -0300

    sdfsdf

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
-axios.defaults.baseURL = 'franco-pi.up.railway.app/'
+axios.defaults.baseURL = 'franco-pi.up.railway.app'
 
 ReactDOM.render(
   <React.StrictMode>
