9e1b6c8da01fff08a46f24c76812391bec2fbeb9
franco <francoadrian.coll2012@gmail.com>
2022-12-15 18:08:19 -0300

    cambios en front

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Landing.module.css'
+import fondo from '../../assets/fondo.png'
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
+
 
 const Landing = () => {
   return (
-      <div className={styles.back}>
-        <Link to={'/home'}>
-          <button className={styles.start}>
-            Start!
-          </button>
-        </Link>
+    <div className={styles.background}>
+      <div className={styles.contIzq}>
+        <div className={styles.logo}>
+          <a
+            href="https://www.linkedin.com/in/franco-coll/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img src={linkedin} alt="linkedin" className={styles.linkedin} />
+          </a>
+
+          <a href="https://github.com/francocoll" target="_blank" rel="noreferrer">
+            {" "}
+            <img src={github} alt="github" className={styles.github} />
+          </a></div>
+        <div className={styles.contTitle}>
+          <div className={styles.title}>¡Welcome to the Dogs App!</div>
+          <Link to="/home">
+            <button className={styles.button}>START</button>
+          </Link>
+        </div>
       </div>
+      <div className={styles.contDer}>
+        <img src={fondo} alt="perro" className={styles.dogImg} />
+      </div>
+    </div>
   )
 }
 
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,8 +3,10 @@ import { useDispatch } from "react-redux";
 import { Link } from 'react-router-dom'
 import { getDogName, getDogs } from '../../actions'
 import styles from './SearchBar.module.css'
+import search from '../../assets/search.png'
+import add from '../../assets/add.png'
 
-const SearchBar = ({setCurrentPage}) => {
+const SearchBar = ({ setCurrentPage }) => {
     const dispatch = useDispatch()
     const [name, setName] = useState('')
 
@@ -29,19 +31,31 @@ const SearchBar = ({setCurrentPage}) => {
         } else {
             alert('Write a name!')
         }
-        
+
     }
 
     return (
         <div className={styles.box}>
-            <div>
-                <input id='search' type='text' placeholder='Search a dog' onChange={(e) => handleInputChange(e)} />
-                <button type='submit' onClick={(e) => handleSubmit(e)}>Search</button>
+            <div className={styles.barra}>
+                <input
+                    id='search'
+                    type='text'
+                    placeholder='Search a dog'
+                    onChange={(e) => handleInputChange(e)} />
+                <button
+                    type='submit'
+                    onClick={(e) => handleSubmit(e)}
+                    className={styles.searchButton}
+                >
+                    <img src={search} alt='search' className={styles.search} />
+                </button>
             </div>
-            <button className={styles.reload} onClick={(e) => { handleClick(e) }}>Reload</button>
-            <Link to='/create'>
-                <button className={styles.add}>Create dog</button>
-            </Link>
+            <div className={styles.botones}>
+                <Link to='/create'>
+                    <img src={add} alt='add' className={styles.add} />
+                </Link>
+                <button className={styles.reload} onClick={(e) => { handleClick(e) }}>Reload</button>
+            </div>
         </div>
     )
 }
