bf4cc694f68d86109abbbf92476b489335c04ac5
franco <francoadrian.coll2012@gmail.com>
2022-12-19 13:58:26 -0300

    search bar change

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,14 @@ import Home from './components/Home/Home'
 import Landing from './components/Landing/Landing'
 import Create from './components/Create/Create';
 import Detail from './components/Detail/Detail';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
+      {/* <SearchBar
+        setCurrentPage={setCurrentPage}
+      /> */}
       <Routes>
         <Route exact path='/' element={<Landing />} />
         <Route exact path='/home' element={<Home />} />
