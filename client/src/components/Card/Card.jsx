6c507d01828e86a627a5a42e3f706e740ff4e54d
Franco Coll <91277981+francocoll@users.noreply.github.com>
2023-01-27 13:13:48 -0300

    form changes

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styles from './Card.module.css'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -8,35 +7,18 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
 const DogCard = ({ name, image, id, weightMin, weightMax, temperament }) => {
-
-
   return (
-    // <div className={styles.fullCard}>
-    //   <Link to={`/dogs/${id}`}>
-    //     <div className={styles.card}>
-    //       <div className={styles.cardHeader}>
-    //         <img className={styles.image} src={image} alt='Dog' />
-    //       </div>
-    //       <div className={styles.info}>
-    //         <h4>{name}</h4>
-    //         <span>{weightMin} - {weightMax} kg</span>
-    //         <p>
-    //         {temperament}
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </Link>
-    // </div>
     <Card sx={{ maxWidth: 395 }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="220"
-          width="100"
-          image={image}
-          alt="green iguana"
-          
-        />
+        <Link to={`/dogs/${id}`}>
+          <CardMedia
+            component="img"
+            height="220"
+            width="100"
+            image={image}
+            alt="green iguana"
+          />
+        </Link>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {name}
